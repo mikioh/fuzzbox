32fe92cc7ce3907b6539a758f55726fcf1a46eba
Mikio Hara <mikioh.mikioh@gmail.com>
2016-07-20 12:28:41 +0900

    fuzzbox/route: add test for golang.org/x/net/route

---
+++ route/route.go
@@ -0,0 +1,21 @@
+// Copyright 2016 Mikio Hara. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package route
+
+import "golang.org/x/net/route"
+
+func Fuzz(data []byte) int {
+	var lastErr error
+	for i := 0; i < 64; i++ {
+		if _, err := route.ParseRIB(route.RIBType(i), data); err != nil {
+			lastErr = err
+			continue
+		}
+	}
+	if lastErr != nil {
+		return 0
+	}
+	return 1
+}
